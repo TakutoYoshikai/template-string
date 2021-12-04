b08ba4a4ad2725d266a5520c44010ef30004f5e9
takuto <takuto.yoshikai@gmail.com>
2021-12-04 09:28:32 +0900

    throw error

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ function renderTemplate (template, data) {
   for (const key of keys) {
     result = result.replaceAll("{" + key + "}", data[key]);
   }
+  let remain;
+  if ((remain = result.match(/\{[a-z]{1,}\}/)) !== null) {
+    throw new Error(`The rendering needs ${remain} value.`);
+  }
   return result;
 }
+
+console.log(renderTemplate("{hello}, {world}, {hoge}", {
+//  hello: "hey",
+  world: "word",
+  hoge: "fuga",
+}));
 
 module.exports = renderTemplate;
